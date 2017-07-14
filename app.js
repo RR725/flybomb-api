8bb372bac6cbd237cd9252c57894587d18455bdc
RR725 <275537913@qq.com>
2017-07-14 21:27:45 +0800

    random get question

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ app.post('/restapi/flybomb/subject/list', subject.list);
 app.post('/restapi/flybomb/question/add', question.add);
 app.post('/restapi/flybomb/question/update', question.update);
 app.post('/restapi/flybomb/question/list', question.list);
+app.post('/restapi/flybomb/question/list/random', question.listRandom);
+
 app.post('/restapi/flybomb/question/find/one', question.findOne);
 
 // catch 404 and forward to error handler
