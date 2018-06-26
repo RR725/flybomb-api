055ba510f4465a61e886d7d914c0ce1b58b36c74
RR725 <kaifei@meizu.com>
2018-06-26 18:03:03 +0800

    common fun

--- controller/restapi/common.js
+++ controller/restapi/common.js
@@ -1,19 +1,20 @@
+module.exports = function(req, res) {
+  console.log(req.session.uid)
+  let sess = req.session
+  let userName = sess.uid
 
-module.exports = function(req,res,callback) {
-	const body = req.body;
-	let loginCount = req.session.loginCount || 0;
-	let lastLoginTime = req.session.lastLoginTime || 0;
-	const now = Date.now();
-	req.session.lastLoginTime = now;
-	req.session.loginCount = loginCount + 1;
-	if (loginCount > 8 && now - lastLoginTime < 300 * 1000) {
-		res.send({
-			code: "500",
-			value: null,
-			message: "操作太频繁"
-		});
-		return;
-	}
-	callback(req,res);
-
-};
+  const body = req.body
+  let loginCount = sess.loginCount || 0
+  let lastLoginTime = sess.lastLoginTime || 0
+  const now = Date.now()
+  req.session.lastLoginTime = now
+  req.session.loginCount = loginCount + 1
+  if (loginCount > 8 && now - lastLoginTime < 300 * 1000) {
+    res.send({
+      code: '500',
+      value: null,
+      message: '操作太频繁'
+    })
+    return
+  }
+}
