a7d69f771d98ae22af4f38af4e2276b4f618ab9f
System Administrator <root@ecofedeMacBook-Air.local>
2018-10-28 14:51:06 +0800

    修改操作繁忙时间

--- controller/restapi/common.js
+++ controller/restapi/common.js
@@ -1,26 +1,27 @@
-module.exports = function(req, res,next) {
-  let sess = req.session
-  let userName = sess.uid
+module.exports = function(req, res, next) {
+    let sess = req.session;
+    let userName = sess.uid;
 
-  const body = req.body
-  let operateCount = sess.operateCount || 0
-  const now = Date.now()
-  let lastOperateTime = sess.lastOperateTime || 0
-  if(operateCount % 50 === 0){
-    req.session.lastOperateTime = now
-  }
-  req.session.operateCount = operateCount + 1
-  if (operateCount > 50 && now - lastOperateTime < 300 * 1000) {
-    res.send({
-      code: '500',
-      value: null,
-      message: '操作太频繁'
-    })
-    return
-  }
-  // if(1){
-  //   res.send({code:3});
-  //   return;
-  // }
-  next();
-}
+    const body = req.body;
+    let operateCount = sess.operateCount || 0;
+    const now = Date.now();
+    let lastOperateTime = sess.lastOperateTime || 0;
+    if (operateCount % 50 === 0) {
+        req.session.lastOperateTime = now;
+    }
+
+    req.session.operateCount = operateCount + 1;
+    if (operateCount > 50 && now - lastOperateTime < 60 * 1000) {
+        res.send({
+            code: '500',
+            value: null,
+            message: '操作太频繁'
+        });
+        return;
+    }
+    // if(1){
+    //   res.send({code:3});
+    //   return;
+    // }
+    next();
+};
