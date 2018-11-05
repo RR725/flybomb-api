078674db779f2bc88198fc2102aeaadc0a9ca76a
System Administrator <root@ecofedeMacBook-Air.local>
2018-11-05 21:19:40 +0800

    操作繁忙逻辑调整

--- controller/restapi/common.js
+++ controller/restapi/common.js
@@ -5,18 +5,22 @@ module.exports = function(req, res, next) {
     const body = req.body;
     let operateCount = sess.operateCount || 0;
     const now = Date.now();
-    if (operateCount % 50 === 0) {
+    if (operateCount === 0) {
         req.session.initOperateTime = now;
     }
     req.session.operateCount = operateCount + 1;
-    if (operateCount > 50 && now - req.session.initOperateTime < 60 * 1000) {
-        res.send({
-            code: '500',
-            value: null,
-            message: '操作太频繁'
-        });
-        return;
+    if (operateCount === 50) {
+        req.session.operateCount = 0;
+        if (now - req.session.initOperateTime < 60 * 1000) {
+            res.send({
+                code: '500',
+                value: null,
+                message: '操作太频繁'
+            });
+            return;
+        }
     }
+
     // if(1){
     //   res.send({code:3});
     //   return;
